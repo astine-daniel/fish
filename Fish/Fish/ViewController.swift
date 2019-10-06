a71c0e90b9d613440b29ae24a3bbc815be8797c8
Daniel Astine <daniel@astine.com.br>
2019-10-06 17:25:41 -0300

    Add and setup SwiftLint

--- Fish/Fish/ViewController.swift
+++ Fish/Fish/ViewController.swift
@@ -1,20 +1,7 @@
-//
-//  ViewController.swift
-//  Fish
-//
-//  Created by Daniel Astine on 06/10/19.
-//  Copyright © 2019 Daniel Astine. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
